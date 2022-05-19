84396e00b08f0f4ecf09095ec936858dc68350c7
alanteles99 <99844374+alanteles99@users.noreply.github.com>
2022-05-18 21:53:17 -0300

    Aula 18/05/2022

---
+++ Aula 18-05-2022/Javascript/2.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#botao").click(function(){
+        $("texto").css("color","blue");
+        $("texto").css("text-decoration","underline");
+        $("texto").css("cpadding","10px");
+        $("texto").css("backgroud-color","#ccc");
+    });
+});
---
+++ Aula 18-05-2022/Javascript/script.js
@@ -0,0 +1,5 @@
+/*jQuery*/
+$('#div1').text("Olá! Bem vindo ao jQuery");
+
+/*jQuery text() Method
+Os text() método define ou retorna o conteúdo de texto dos elementos selecinados. Quando este método é usado para retornar o conteúdo, ele retorna o conteúdo de texto de todos os elementos combinados (marcação HTML será remoido).*/
